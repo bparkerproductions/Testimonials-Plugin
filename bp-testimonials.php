79fcb835daeca28c86ff14e6bbfdc88912e963eb
Brandon Parker <bparkerproductions@gmail.com>
2023-01-03 15:35:12 -0800

    fix author name

--- bp-testimonials.php
+++ bp-testimonials.php
@@ -6,7 +6,7 @@
 * Version: 1.0
 * Requires at least: 5.6
 * Requires PHP: 7.0
-* Author: Marcelo Vieira
+* Author: Brandon Parker
 * License: GPL v2 or later
 * License URI: https://www.gnu.org/licenses/gpl-2.0.html
 * Text Domain: bp-testimonials
